dac7a162d30222c74cb97ade76bd738815653313
aalsharbini <89406314+aalsharbini@users.noreply.github.com>
2022-10-14 21:01:53 -0500

    added files

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+function validate() {
+  var empt = document.getElementById("email").value;
+  console.log("i'm working fine here");
+  if (empt == "") {
+    alert("Please Enter Your Email");
+  }
+}
